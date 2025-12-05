a61f80dc89f2caa04c3551c8e284bbd6821d87e2
ryanlin1234 <rylin@chapman.edu>
2025-12-04 20:56:15 -0800

    Add files via upload

--- src/sn_discography_data_cleaning.R
+++ src/sn_discography_data_cleaning.R
@@ -1,1 +1,21 @@
+# Script: sn_discography_data_cleaning.R
+# Description: Cleaning data for Stevie Nicks project
 
+library(readr)
+library(dplyr)
+library(lubridate)
+library(tidyr)
+library(janitor)
+
+sn_discography <- read.csv("../data/StevieNicksAlbumTracks.csv")
+
+sn_discography_clean <- sn_discography %>%
+  mutate(
+    duration_seconds = period_to_seconds(ms(Duration)),
+    duration_minutes = duration_seconds / 60,
+    key = factor(Key),
+    musical_mode = factor(Mode),
+    album = factor(Album),
+    decade = paste0(floor(Year/10)*10, "s")
+  ) %>%
+  arrange(Year, Album)
